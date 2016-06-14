2e24433dbc6efee2e48fa5ede40f1ad0269b9d2a
Jason Trader <jasonmtrader@gmail.com>
2016-06-14 12:16:04 -0700

    Finished program before testing
    pretty standard, all varables need to be input and there is no error checking on tstep entry

--- TimeDiffusionCartesian.cpp
+++ TimeDiffusionCartesian.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+
+bool steadyState(double oldt[], double newt[], double thresh, int ndx){
+  double totThresh = ndx*thresh;
+  double sum = 0;
+  for(int i = 1; i < ndx - 1; i++){
+    double temp = newt[i] - oldt[i];
+    if (temp < 0)
+      temp *= -1;
+    sum += temp;
+  }
+  return (sum < totThresh);
+}
 
 int main(){
   //lx is length of rod ndx is the number of segments and ldx is the length of each segment.
@@ -15,11 +29,39 @@ int main(){
   cin >> tleft;
   cout << "What is the teperature of the right side of the rod for all time? ";
   cin >> tright;
-  cout << "Initial conditions:" << endl;
-  double oldt, newt[ndx];
+  cout << "what is your diffusitivity constant? ";
+  double difus;
+  cin >> difus;
+  cout << "What is you time step? (Must be smaller than " << setprecision(3) << (ldx*ldx)/(2*difus) << "s to ensure stability)" ;
+  double tstep;
+  cin >> tstep;
+  cout << "What must the average change be to consider the solution as steady? ";
+  double thresh;
+  cin >> thresh;
+  cout << "Initial conditions\n-----------------------" << endl;
+  double oldt [ndx];
+  double newt[ndx];
   oldt[0] = tleft;
   oldt[ndx - 1] = tright;
   newt[0] = tleft;
   newt[ndx - 1] = tright;
-  
+
+  for(int inc = 1; inc < ndx - 1; inc++){
+    cout << "Segemt " << inc << ": ";
+    cin >> oldt[inc];
+  }
+  int tcount = 0;
+  while(!steadyState(oldt, newt, thresh, ndx)){
+    tcount++;
+    for(int i = 1; i < (ndx - 1); i++){
+      newt[i] = -2*oldt[i];
+      newt[i] += oldt[i + 1];
+      newt[i] += oldt[i - 1];
+      newt[i] *= (difus*tstep/(ldx*ldx));
+      newt[i] += oldt[i];
+      oldt[i] = newt[i];
+    }
+  }
+  cout << "\n---------------------------------\n";
+  cout << "Solution converged in " << tcount * tstep << " seconds\n";
 }
