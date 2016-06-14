1cb9cb9eb780360e6e52ea0355fda3d2b490c036
Jason Trader <jasonmtrader@gmail.com>
2016-06-14 14:01:49 -0700

    Seems to be converging correctly

--- TimeDiffusionCartesian.cpp
+++ TimeDiffusionCartesian.cpp
@@ -32,7 +32,7 @@ int main(){
   cout << "what is your diffusitivity constant? ";
   double difus;
   cin >> difus;
-  cout << "What is you time step? (Must be smaller than " << setprecision(3) << (ldx*ldx)/(2*difus) << "s to ensure stability)" ;
+  cout << "What is you time step? (Must be smaller than " << (ldx*ldx)/(2*difus) << "s to ensure stability) " ;
   double tstep;
   cin >> tstep;
   cout << "What must the average change be to consider the solution as steady? ";
@@ -48,20 +48,20 @@ int main(){
 
   for(int inc = 1; inc < ndx - 1; inc++){
     cout << "Segemt " << inc << ": ";
-    cin >> oldt[inc];
+    cin >> newt[inc];
   }
   int tcount = 0;
   do{
     tcount++;
     cout << oldt[0] << " ";
     for(int i = 1; i < (ndx - 1); i++){
+      oldt[i] = newt[i];
       newt[i] = -2*oldt[i];
       newt[i] += oldt[i + 1];
       newt[i] += oldt[i - 1];
       newt[i] *= (difus*tstep/(ldx*ldx));
       newt[i] += oldt[i];
       cout << oldt[i] << " ";
-      oldt[i] = newt[i];
     }
     cout << oldt[ndx - 1] << endl;
   } while(!steadyState(oldt, newt, thresh, ndx));
