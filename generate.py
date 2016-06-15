34538958040ff1dd165184e72cb16210ab5b52c2
Jason Trader <jasonmtrader@gmail.com>
2016-06-15 09:03:00 -0700

    Generate file for input params

---
+++ generate.py
@@ -0,0 +1,12 @@
+f = open('input', 'w')
+f.write(str(1) + '\n')
+f.write(str(100) + '\n')
+f.write(str(50) + '\n')
+f.write(str(100) + '\n')
+f.write(str(0.01) + '\n')
+f.write(str(0.001) + '\n')
+f.write(str(0.000001) + '\n')
+for i in range(50):
+    f.write(str(50) + '\n')
+for i in range(50):
+    f.write(str(0) + '\n')
