93dc2fdcdb6f1ac137fc7ed39e9a38c632456ec5
Jason Trader <jasonmtrader@gmail.com>
2016-06-20 16:37:47 -0700

    Changed generate file to incorporate result output

--- TimeDiffusion1D-Cartesian/generate.py
+++ TimeDiffusion1D-Cartesian/generate.py
@@ -5,6 +5,7 @@ f.write(str(50) + '\n')
 f.write(str(100) + '\n')
 f.write(str(0.01) + '\n')
 f.write(str(0.001) + '\n')
+f.write(str(1) + '\n')
 f.write(str(0.000001) + '\n')
 for i in range(50):
     f.write(str(50) + '\n')
