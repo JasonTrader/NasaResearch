9d6525f138e1c584b690590c08fecedb01377572
Jason Trader <jasonmtrader@gmail.com>
2016-06-23 10:59:49 -0700

    Updated distributed code
    Seems to be working

--- MagneticDiffusion1D-Polar/distributed/rundist.cu
+++ MagneticDiffusion1D-Polar/distributed/rundist.cu
@@ -15,30 +15,17 @@
 
 
 #include "book.h"
+#include "func.h"
 #include <stdio.h>
 
 #define PI 3.1415926535897932384
 #define mu0 4*PI*1e-7
 
-__global__ void init(double *rod_new, double imax, double ldr, double rlength, int totl) {
-  int i = blockIdx.x;
-  if (i < totl - 1){
-    if(i != 0)
-      rod_new[i] = (1-(i*i*ldr*ldr/(3*rlength*rlength)))*3*mu0*imax*i*ldr/(4*PI*rlength*rlength);
-  }
-}
-
-__global__ void copyToOld(double *rod_new, double *rod_old, int totl){
-  int i = blockIdx.x;
-  if (i < totl - 1)
-    rod_old[i] = rod_new[i];
-}
-
 int main( void ) {
 
   FILE *myfile;
   myfile = fopen("results.txt", "w");
-  double imax, rlength, eta, tstep, ldr, conv;
+  double imax, rlength, eta, tstep, ldr, thresh;
   int numseg;
 
   printf("%s", "What is your I max? ");
@@ -54,41 +41,68 @@ int main( void ) {
   printf("%s%lf%s", "What time step would you like? (must be less than ", bound, " ) ");
   scanf("%lf", &tstep);
   printf("%s", "What is the threshold for your convergence? ");
-  scanf("%lf", &conv);
+  scanf("%lf", &thresh);
 
   //initialize
   double *rod_new = new double[numseg+2];
-  double *rod_old = new double[numseg+2];
-  *rod_new = 0;
-  *(rod_new + numseg + 1) = mu0*imax/(2*PI*rlength);
-  *rod_old = 0;
-  *(rod_old + numseg + 1) = mu0*imax/(2*PI*rlength);
+  //double *rod_old = new double[numseg+2];
+  bool *conv = new bool[numseg];
+  //*rod_new = 0;
+  //*(rod_new + numseg + 1) = mu0*imax/(2*PI*rlength);
+  //*rod_old = 0;
+  //*(rod_old + numseg + 1) = mu0*imax/(2*PI*rlength);
 
   double *dev_old, *dev_new;
+  bool *dev_conv;
 
   // allocate the memory on the GPU
   HANDLE_ERROR( cudaMalloc( (void**)&dev_old, (numseg+2) * sizeof(double) ) );
   HANDLE_ERROR( cudaMalloc( (void**)&dev_new, (numseg+2) * sizeof(double) ) );
+  HANDLE_ERROR( cudaMalloc( (void**)&dev_conv, numseg * sizeof(bool) ) );
 
-  // fill the arrays 'old' and 'new'
-  init<<<numseg+1,1>>>(dev_new, imax, ldr, rlength, numseg+2);
+  // fill the array 'new'
+  init<<<numseg+2,1>>>(dev_new, imax, ldr, rlength, numseg+2);
 
-  // copy the array 'c' back from the GPU to the CPU
+  // copy data on device from 'dev_new' to 'rod_new'
   HANDLE_ERROR( cudaMemcpy( rod_new, dev_new, (numseg+2) * sizeof(double), cudaMemcpyDeviceToHost ) );
 
-  // output x values
-  for (int i=0; i<numseg+2; i++) {
-      fprintf( myfile, "%lf ", i*ldr );
-  }
 
-  for (int i=0; i<numseg+2; i++) {
-      fprintf( myfile, "%lf ", *(rod_new+i) );
+  int out;
+  // output r values
+  for (out = 0; out<numseg+1; out++) {
+      fprintf( myfile, "%lf ", out*ldr );
   }
+  fprintf( myfile, "%lf\n", out*ldr );
+
+  double aug = eta*tstep/(mu0*ldr*ldr);
+  int tcount = 0;
+
+  do{
+    if(tcount%100==0){
+      for (out=0; out<numseg+1; out++) {
+          fprintf( myfile, "%lf ", *(rod_new+out) );
+      }
+      fprintf( myfile, "%lf\n", *(rod_new+out) );
+    }
+    tcount++;
+
+    //copy new to old
+    copyToOld<<<numseg+2,1>>>(dev_new, dev_old, numseg+2);
+
+
+
+    //update
+    update<<<numseg,1>>>(dev_new, dev_old, numseg+1, dev_conv, thresh, aug);
+    HANDLE_ERROR( cudaMemcpy( rod_new + 1, dev_new + 1, numseg * sizeof(double), cudaMemcpyDeviceToHost ) );
+    HANDLE_ERROR( cudaMemcpy( conv, dev_conv, numseg, cudaMemcpyDeviceToHost ) );
+
+  } while(!converge(conv, numseg));
 
   // free the memory allocated on the GPU
   HANDLE_ERROR( cudaFree( dev_old ) );
   HANDLE_ERROR( cudaFree( dev_new ) );
 
+  fprintf(myfile, "STOP\n");
   fclose(myfile);
 
   return 0;
