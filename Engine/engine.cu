f08ad0bcb75043fc68e5350ec55d1ea2569c14a4
Jason Trader <jasonmtrader@gmail.com>
2016-07-28 15:21:23 -0700

    Outline of what needs to be done

--- Engine/engine.cu
+++ Engine/engine.cu
@@ -3,36 +3,30 @@
 
 #define PI 3.1415926535897932384
 #define mu0 4*PI*1e-7
-//Threads per block is capped at 1024 for hardware reasons
-//In some cases using a smaller number of threads per block will be more efficient
-#define threadsPerBlock 1024
-//Max grid points is to defined in order to allocate shared memory for each block
-#define MaxGridPoints 6144
-
 
 int main(){
-  int numProblems;
-  scanf("%d", &numProblems);
 
-  for(int i =0; i< numProblems; i++){
-    //TODO getInitial
+  //TODO get input
+
+  //TODO calculate initial conserved quantities
+
+  //Time loop
+  while(t < totTime){
+
+    //TODO Calculate electric potential
+
+    //TODO calculate fluxes
+
+    //TODO Update conserved quamtities
+
+    //TODO Update secondary quantities
+
+
   }
-
-  //TODO Copy needed memory down
-
-  //TODO Run Init function
-
-  //TODO Copy back orignal conditions for output maybe?
-
-  //TODO output inital to file maybe?
-
-  //TODO run simulation
-
-  //TODO copy back results
 
   //TODO Output results
 
   //TODO free memory
-  
+
   return 0;
 }
