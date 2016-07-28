a2f45680a9f38611bf5a88489fb56684ec524486
Jason Trader <jasonmtrader@gmail.com>
2016-07-28 15:43:48 -0700

    Get initial conditions

--- Engine/engine.cu
+++ Engine/engine.cu
@@ -6,7 +6,47 @@
 
 int main(){
 
-  //TODO get input
+  //-------------------------------------------------------------------------//
+  //get input
+
+  //Propellant (AMU) (currently not used)
+  scanf("%*s %*s %*s %*d");
+
+  //Mass flow rate (kg/s) (currently not used)
+  scanf("%*s %*s %*s %*lf");
+
+  //inner r (m)
+  double rIn;
+  scanf("%*s %*s %*s %lf", &rin);
+
+  //outer r (m)
+  double rOut;
+  scanf("%*s %*s %*s %lf", &rout);
+
+  //Total rlength
+  double lr = rout - rin;
+
+  //z length (m)
+  double lz;
+  scanf("%*s %*s %*s %lf", &lz);
+
+  //number of points in r direction
+  int nr;
+  scanf("%*s %*s %*s %d", &nr);
+
+  //number of points in z direction
+  int nz;
+  scanf("%*s %*s %*s %d", &nz);
+
+  //Start time (s)
+  double startTime;
+  scanf("%*s %*s %*s %lf", &startTime);
+
+  //End time (s)
+  double endTime;
+  scanf("%*s %*s %*s %lf", &endTime);
+
+//---------------------------------------------------------------------------//
 
   //TODO calculate initial conserved quantities
 
