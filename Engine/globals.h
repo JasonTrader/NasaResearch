7dc37deb143bbee438029fc0221d5036cae4814b
Jason Trader <jasonmtrader@gmail.com>
2016-07-29 09:08:21 -0700

    Added comment

--- Engine/globals.h
+++ Engine/globals.h
@@ -1,6 +1,9 @@
 #ifndef _GLOBALS_H_
 #define _GLOBALS_H_
 
+//NOTE globals should be declared in all caps to denote the fact that they are
+//globals
+
 #define PI 3.1415926535897932384
 #define MU0 4*PI*1e-7
 #define R_EVALS_PER_BLOCK 16
