367450cbf3a5cf6d41d1737b21bff87865f5e418
Jason Trader <jasonmtrader@gmail.com>
2016-07-29 10:25:46 -0700

    Added update electric potential

--- Engine/engine.cu
+++ Engine/engine.cu
@@ -52,11 +52,42 @@ int main(){
 
   //TODO calculate secondary initial quantities
 
+
+//---------------------------------------------------------------------------//
+//This area is used for thread/block dim calcs
+
+  dim3 cornerGridWHalosThreadDim(R_EVALS_PER_BLOCK+2, Z_EVALS_PER_BLOCK+2);//+ 2 accounts for halo points
+  int cornerGridWHalosR = 1 + (nr-1)/R_EVALS_PER_BLOCK;//nr = number internal r grid points
+  int cornerGridWHalosZ = 1 + (nz-1)/Z_EVALS_PER_BLOCK;//nz = number internal z grid points
+  dim3 cornerGridWHalosBlockDim(cornerGridWHalosR, cornerGridWHalosZ);
+
+
   //Time loop
   double t = startTime;
   while(t < endTime){
 
-    //TODO Calculate electric potential
+
+//---------------------------------------------------------------------------//
+//Update Voltage
+    bool didConverge = false;
+    while(!didConverge){
+      cudaMemcpy(volt_d_old, volt_d_new, gridSize, cudaMemcpyDeviceToDevice);
+      //Evaluate red blocks
+      updateVoltage<<<cornerGridWHalosBlockDim,cornerGridWHalosThreadDim,(zEvalsPerBlock+2)*(rEvalsPerBlock+2)*sizeof(double)>>>(volt_d_old, volt_d_new, true, converge_d, nr, nz, dr, dz, blockz);
+      cudaMemcpy(volt_d_old, volt_d_new, gridSize, cudaMemcpyDeviceToDevice);
+      //Evaluate black blocks
+      updateVoltage<<<cornerGridWHalosBlockDim,cornerGridWHalosThreadDim,(zEvalsPerBlock+2)*(rEvalsPerBlock+2)*sizeof(double)>>>(volt_d_old, volt_d_new, false, converge_d, nr, nz, dr, dz, blockz);
+      //copy back converge check
+      cudaMemcpy(converge_h, converge_d, convSize, cudaMemcpyDeviceToHost);
+
+      //all converge must be true
+      didConverge = converge_h[0];
+      for(int i = 1; i< 2*blockr*blockz; i++){
+        didConverge = didConverge && converge_h[i];
+      }
+    }//converged
+
+//---------------------------------------------------------------------------//
 
     //TODO calculate fluxes
 
@@ -64,7 +95,7 @@ int main(){
 
     //TODO Update secondary quantities
 
-
+    t+=dt;//update time
   }
 
   //TODO Output results
