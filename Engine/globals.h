08eb224879f6fc846b5e042f8c16aee74a0b44c4
Jason Trader <jasonmtrader@gmail.com>
2016-07-28 17:27:37 -0700

    File for global variables
    Note: globals will be notated by all caps

---
+++ Engine/globals.h
@@ -0,0 +1,10 @@
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+#define PI 3.1415926535897932384
+#define MU0 4*PI*1e-7
+#define R_EVALS_PER_BLOCK 16
+#define Z_EVALS_PER_BLOCK 16
+#define EPSILON 1e-12
+
+#endif
