4580e3fd7973a4f99550afae958348b463edb0dd
Jason Trader <jasonmtrader@gmail.com>
2016-08-02 10:02:35 -0700

    update globals

--- Engine/globals.h
+++ Engine/globals.h
@@ -9,6 +9,12 @@
 #define R_EVALS_PER_BLOCK 16
 #define Z_EVALS_PER_BLOCK 16
 #define EPSILON 1e-12
+//TODO update these
+#define kBoltz 1.38064852e-23
+#define Tp 20000
+#define Tn 20000
+#define Mp 1.6726219e-27
+#define Mn 1.6726219e-27
 
 enum U{
   massP,
