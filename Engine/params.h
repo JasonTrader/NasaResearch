b7650698c8b77889c9fbf9b1101932026ff82eb3
Jason Trader <jasonmtrader@gmail.com>
2016-08-03 15:53:08 -0700

    name change

---
+++ Engine/params.h
@@ -0,0 +1,48 @@
+#ifndef _PARAMS_H_
+#define _PARAMS_H_
+
+void getParams(int *atomicMass, double *rIn, double *rOut, double *lr, double *lz,
+   int *nr, int *nz, double *startTime, double *endTime, double *dr, double *dz, double *vMax, double *vMin){
+
+  //Propellant (AMU)
+  scanf("%*s %*s %*s %d", atomicMass);
+
+  //Mass flow rate (kg/s) (currently not used)
+  scanf("%*s %*s %*s %*lf");
+
+  //inner r (m)
+  scanf("%*s %*s %*s %lf", rIn);
+
+  //outer r (m)
+  scanf("%*s %*s %*s %lf", rOut);
+
+  //Total rlength
+  *lr = *rOut - *rIn;
+
+  //z length (m)
+  scanf("%*s %*s %*s %lf", lz);
+
+  //number of points in r direction
+  scanf("%*s %*s %d", nr);
+
+  //number of points in z direction
+  scanf("%*s %*s %d", nz);
+
+  //Start time (s)
+  scanf("%*s %*s %*s %lf", startTime);
+
+  //End time (s)
+  scanf("%*s %*s %*s %lf", endTime);
+
+  //Vmax (V)
+  scanf("%*s %*s %*s %lf", vMax);
+
+  //Vmin (V)
+  scanf("%*s %*s %*s %lf", vMin);
+
+
+  *dr = *lr/(*nr+1);
+  *dz = *lz/(*nz+1);
+}
+
+#endif
